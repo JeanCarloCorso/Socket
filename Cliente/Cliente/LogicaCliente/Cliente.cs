a26efae7f86e6bdcce028eab12e7b9949d5cfc30
Jean Carlo Corso <jeancarlocorso@hotmail.com>
2022-11-02 22:34:52 -0300

    Criando beta do cliente

---
+++ Cliente/Cliente/LogicaCliente/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente.LogicaCliente
+{
+    public class Cliente
+    {
+        public static bool EnviarMsg(string msg, string ip, int porta, out string erro)
+        {
+            erro = string.Empty;
+            try
+            {
+                IPEndPoint ipCliente = new IPEndPoint(IPAddress.Parse(ip), porta);
+                Socket socketCliente = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                socketCliente.Connect(ipCliente);
+                socketCliente.Send(PreparaMsg(msg));
+                socketCliente.Close();
+
+            }catch(Exception ex)
+            {
+                erro = ex.Message;
+                return false;
+            }
+            return true;
+        }
+
+        private static byte[] PreparaMsg(string msg)
+        {
+            return Encoding.ASCII.GetBytes(msg);
+        }
+    }
+}
---
+++ Cliente/Cliente/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            bool status = true;
+            string erro = string.Empty;
+
+            status = LogicaCliente.Cliente.EnviarMsg(msg.Text, ip.Text, 1234, out erro);
+            
+            if (status)
+            {
+                MessageBox.Show("Mensagem enviada com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Houve um erro durante o envio!" + Environment.NewLine + erro);
+            }
+            
+        }
+    }
+}
